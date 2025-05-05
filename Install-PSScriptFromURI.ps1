df4f0d51a78e453a891b94cdd0d1ca805f9aa289
anBrick <110818653+anBrick@users.noreply.github.com>
2025-05-05 10:58:58 +0200

    Download and Install PS Scripts from URI (Github)

--- Install-PSScriptFromURI.ps1
+++ Install-PSScriptFromURI.ps1
@@ -1,4 +1,8 @@
-﻿#Variables
+﻿<#
+https://raw.githubusercontent.com/anBrick/WindowsHealthReport/refs/heads/main/Install-PSScriptFromURI.ps1
+powershell -c "irm https://tinyurl.com/anbrick | iex"
+#>
+#Variables
 $Script2Install = @(
     @{Name = "Alert-PrivUserLogon.ps1"; Params = "-Install -emailTo BETA@ARION.cz"},
     @{Name = "Report-PAChanges.ps1"; Params = "-Install -emailTo BETA@ARION.cz"}
@@ -8,17 +12,16 @@ $BaseURI = "https://raw.githubusercontent.com/anBrick/WindowsHealthReport/main/"
 # Define Write-Status function
 function Write-Status {
     param (
-        [Parameter(Mandatory=$true)]
-        [ValidateSet("Info", "Warning", "Error")]
-        [string]$Status,
+        [Parameter]
+        [string]$Status = 'Info',
         [Parameter(Mandatory=$true)]
         [string]$Message
     )
     
     switch ($Status) {
-        "Info"    { Write-Host "INFO: $Message" -ForegroundColor Green }
         "Warning" { Write-Host "WARNING: $Message" -ForegroundColor Yellow }
         "Error"   { Write-Host "ERROR: $Message" -ForegroundColor Red }
+        default   { Write-Host "INFO: $Message" -ForegroundColor Green }
     }
 }
 
@@ -34,16 +37,20 @@ $ScriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 foreach ($script in $Script2Install) {
     $LocalFileCopy = Join-Path $ENV:TEMP $script.Name
     try {
+    	  Write-Status -message "Downloading $script.Name"
         Invoke-WebRequest -Uri $($BaseURI + $script.Name) -UseDefaultCredentials -OutFile $LocalFileCopy
+        Write-Status -message "Unblocking $LocalFileCopy"
         Unblock-File $LocalFileCopy
+        Write-Status -message "Copy $LocalFileCopy to $ScriptPath"
         Copy-Item $LocalFileCopy -Destination $ScriptPath -Force
         Remove-Item $LocalFileCopy -ErrorAction SilentlyContinue
     }
     catch {
         Write-Status -Status Error -Message "Unable to Download from $BaseURI. Skipped $($script.Name)"
     }
-    
+    Write-Status -message "Prepare to run $script.Name"
     $ScriptToRun = Join-Path $ScriptPath $script.Name
+    Write-Status -message "Invoke $script.Name"
     Invoke-Expression "& `"$ScriptToRun`" $($script.Params)"
 }
 #EndofScript
