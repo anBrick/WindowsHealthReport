457d2294d0aa7fe208f34dddee9deee4e28c40d7
anBrick <110818653+anBrick@users.noreply.github.com>
2025-05-21 16:14:03 +0200

    errors fixed

--- Report-PAChanges.ps1
+++ Report-PAChanges.ps1
@@ -34,33 +34,40 @@ Param(
 )
 	switch ($Status) {
 		'Information' {Write-host $Message}
-		'Warning' {Write-host $Message -foregroundColor yellow}
+		'Warning' {Write-Warning $Message -foregroundColor yellow}
 		'Error' {Write-Error $Message}
 	}
-	Write-EventLog -LogName Application -Source "Userenv" -EntryType $Status -EventID 34343 -Message $(( '{0} Runtime message:: {1}') -f $MyInvocation.myCommand.name,$Message) -ea 0 
+	# Log source for Application Event Log
+	$source = [IO.Path]::GetFileNameWithoutExtension($MyInvocation.PSCommandPath)
+	if ([string]::IsNullOrEmpty($source)) {$source = [IO.Path]::GetFileNameWithoutExtension($PSCommandPath)}
+	if (-not [System.Diagnostics.EventLog]::SourceExists($source)) {[System.Diagnostics.EventLog]::CreateEventSource($source, "Application")} #register EvtLog Source
+	Write-EventLog -LogName Application -Source $source -EntryType $Status -EventID 34343 -Message $(( '{0} Runtime message:: {1}') -f $MyInvocation.myCommand.name,$Message) -ea 0 
 }
-#Auto Update Code
+#SELF Update Code
 $ScriptDistributionPoints = @('c:\report\',$($ENV:LOGONSERVER + "\NETLOGON\"),"https://raw.githubusercontent.com/anBrick/WindowsHealthReport/main/") ## path for automatic upgrade from
 foreach ($ScriptDistributionPoint in $ScriptDistributionPoints){
 if ($ScriptDistributionPoint -match "^(https?:\/\/)") {
 	$UpdatesFile = $ENV:TEMP + '\' + $MyInvocation.myCommand.name
-	try {
-		Invoke-WebRequest -Uri $($ScriptDistributionPoint + $MyInvocation.myCommand.name) -UseDefaultCredentials -OutFile $UpdatesFile
-		Unblock-File $UpdatesFile
-		Copy-Item $UpdatesFile -Destination $($MyInvocation.MyCommand.Path) -Force;
-		Remove-Item $UpdatesFile -ea 0
+	try {Invoke-WebRequest -Uri $($ScriptDistributionPoint + $MyInvocation.myCommand.name) -UseDefaultCredentials -OutFile $UpdatesFile
+		Write-Status -Status Information -Message "New version downloaded from $($ScriptDistributionPoint + $MyInvocation.myCommand.name), begin updating itself."
 	}
-	catch {Write-Status -Status Error -Message "ERROR: Unable to install updates from $ScriptDistributionPoint, running as is."}
+	catch {Write-Status -Status Error -Message "ERROR $_ : Unable to downlaod updates from $($ScriptDistributionPoint + $MyInvocation.myCommand.name), running as local version."}
+	Unblock-File $UpdatesFile
+	try {Copy-Item $UpdatesFile -Destination $($MyInvocation.MyCommand.Path) -Force;
+		Write-Status -Status Information -Message "New version installed to $($MyInvocation.MyCommand.Path)."
+	}
+	catch {Write-Status -Status Error -Message "ERROR $_ : Unable to uplaod updates to $($MyInvocation.MyCommand.Path), update failed."}
+	Remove-Item $UpdatesFile -ea 0
 }
 else {
 	if ((Test-Path -PathType Leaf -LiteralPath ($ScriptDistributionPoint + $MyInvocation.myCommand.name)) -and ((Get-Item ($ScriptDistributionPoint + $MyInvocation.myCommand.name)).LastWriteTime.ticks -gt ((Get-Item $MyInvocation.MyCommand.Path).LastWriteTime.ticks)))
 	{
 		Write-Status -Status Information -Message ('The Distribution point has the newest version of the script. Starting Upgrade itself')
 		try { Copy-Item ($ScriptDistributionPoint + $MyInvocation.myCommand.name) -Destination $($MyInvocation.MyCommand.Path) -Force; }
-		catch { Write-Status -Status Error -Message  "ERROR: Impossible to upgrade the script from the $ScriptDistributionPoint, leaving it as is." }
+		catch { Write-Status -Status Error -Message  "ERROR $_ : Impossible to upgrade the script from the $ScriptDistributionPoint, leaving it as is." }
 	}
 }
-}
+}#END SelfUpdate
 # Register Scheduled Task (if -Install is used)
 if ($Install) {
     try {
